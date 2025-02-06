71891fc7f6e45e869f87cd1b34a236d7e5cd1e7c
Leysly-Mendoza <leyslyymendozaf@gmail.com>
2025-02-06 09:09:31 -0600

    Http

--- Parcial1/ServidorHttp/index.js
+++ Parcial1/ServidorHttp/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 let server = http.createServer(function (req, res) {
     res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
+    //res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('hello world!');
 });
 
